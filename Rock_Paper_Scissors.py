948bbe39990645c6e790e9e905448b73e1d9fc9f
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-07 17:39:33 -0800

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,13 +1,7 @@
-"""
-LESSON: 2.3 - Booleans
-EXERCISE: Rock Paper Scissors
-"""
-
 #### ---- PLAYER INPUT ---- ####
 
 # Ask player 1 to input rock, paper, or scissors, and
 # assign to a variable called player1
-# ---> TEST AFTER THIS LINE <--- #
 player1 = input("Player 1, please choose rock, paper, or scissors: ")
 
 # BONUS: "\n" is a string that represents a "new line"
@@ -17,7 +11,6 @@ print("\n" * 100)
 
 # Ask player 2 to input rock, paper, or scissors, and
 # assign to a variable called player2
-# ---> TEST AFTER THIS LINE <--- #
 player2 = input("Player 2, please choose rock, paper, or scissors: ")
 
 
@@ -26,7 +19,6 @@ player2 = input("Player 2, please choose rock, paper, or scissors: ")
 # Check whether player1 is EQUAL TO "rock" AND player2
 # is EQUAL TO "scissors". Assign result to a variable
 # rock_beats_scissors.
-# ---> TEST AFTER THIS LINE <--- #
 rock_beats_scissors = player1 == "rock" and player1 == "scissors"
 
 # Check whether player1 is EQUAL TO "scissors" AND
@@ -42,7 +34,6 @@ paper_beats_rock = player1 == "paper" and player2 == "rock"
 # Check whether rock_beats_scissors OR
 # scissors_beats_paper OR paper_beats_rock. Assign
 # result to a variable player1_win.
-# ---> TEST AFTER THIS LINE <--- #
 player1_win = rock+beats_scissor or scissors_beats_paper or paper_beats_rock
 
 
@@ -52,23 +43,16 @@ player1_win = rock+beats_scissor or scissors_beats_paper or paper_beats_rock
 if player1 == player2:
 
     # Print message saying the result was a tie
-    # ---> TEST AFTER THIS LINE <--- #
     print("Tie")
 
 # ELIF player1_win
 elif player1_win:
 
     # Print message saying that player 1 won the game
-    # ---> TEST AFTER THIS LINE <--- #
     print("Player 1 wins!")
 
 # ELSE
 else:
 
     # Print message saying that player 2 won the game
-    # ---> TEST AFTER THIS LINE <--- #
     print("player 2wins!")
-
-
-# Turn in your Coding Exercise.
-
